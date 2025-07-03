f23201c181ed08062074ec6fa10ac1ef95668a82
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-07-03 23:46:46 +0300

    add function investment_bank in module src/services.py

--- src/services.py
+++ src/services.py
@@ -1,13 +1,29 @@
 from datetime import datetime
-from typing import List, Dict, Any
+from typing import Any, Dict, List
 
 import pandas as pd
 
-from src.utils import read_xlsx_file
 
+def investment_bank(month: str, transactions: List[Dict[str, Any]], limit: int) -> float | str:
+    """Высчитывает сумму, которую мог бы накопить пользователь за указанный месяц, учитывая порог округления"""
 
-def investment_bank(month: str, transactions: List[Dict[str, Any]], limit: int) -> float:
-    ...
+    if limit not in [10, 50 , 100]:
+        raise ValueError("Неверно указан порог округления. Доступные варианты - 10, 50, 100")
+
+    result_amount = 0
+
+    try:
+        target_month = datetime.strptime(month, '%Y-%m').month
+        target_year = datetime.strptime(month, '%Y-%m').year
+    except ValueError:
+        return "Неверно указана дата поиска"
+
+    for item in transactions:
+        date_transactions = datetime.strptime(item["Дата операции"], '%Y-%m-%d')
+        if date_transactions.month == target_month and date_transactions.year == target_year:
+            result_amount += limit - (abs(item["Сумма операции"]) % limit)
+
+    return round(result_amount, 2)
 
 
 def preparing_list_transactions(my_list_transactions: pd.DataFrame) -> List[Dict[str, Any]]:
