fd49bd7ce8d87e579d087f236091f216970fddd8
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-07-04 00:22:50 +0300

    add logging in module src/services.py

--- src/services.py
+++ src/services.py
@@ -1,13 +1,25 @@
+import logging
+import os
 from datetime import datetime
 from typing import Any, Dict, List
 
 import pandas as pd
 
+log_path = os.path.join(os.path.dirname(__file__), '..', 'logs', 'services.log')
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler(log_path, mode='w', encoding='utf-8')
+file_formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
 
 def investment_bank(month: str, transactions: List[Dict[str, Any]], limit: int) -> float | str:
     """Высчитывает сумму, которую мог бы накопить пользователь за указанный месяц, учитывая порог округления"""
 
-    if limit not in [10, 50 , 100]:
+    if limit not in [10, 50, 100]:
+        logger.error("Неверно указан порог округления")
         raise ValueError("Неверно указан порог округления. Доступные варианты - 10, 50, 100")
 
     result_amount = 0
@@ -16,12 +28,15 @@ def investment_bank(month: str, transactions: List[Dict[str, Any]], limit: int) 
         target_month = datetime.strptime(month, '%Y-%m').month
         target_year = datetime.strptime(month, '%Y-%m').year
     except ValueError:
+        logger.error("Неверно указана дата поиска")
         return "Неверно указана дата поиска"
 
+    logger.info("Начало подсчета суммы накоплений")
     for item in transactions:
         date_transactions = datetime.strptime(item["Дата операции"], '%Y-%m-%d')
         if date_transactions.month == target_month and date_transactions.year == target_year:
             result_amount += limit - (abs(item["Сумма операции"]) % limit)
+    logger.info("Конец подсчета суммы накоплений")
 
     return round(result_amount, 2)
 
@@ -31,9 +46,11 @@ def preparing_list_transactions(my_list_transactions: pd.DataFrame) -> List[Dict
 
     result = []
 
+    logger.info("Подготовка DataFrame")
     df = my_list_transactions[my_list_transactions["Статус"] == "OK"].loc[:, ['Сумма операции', 'Дата операции']]
     df = df[df['Сумма операции'] < 0].reset_index(drop=True)
 
+    logger.info("Начало формирования списка из дат и сумм транзакций")
     for index in range(len(df)):
         date = datetime.strptime(df.loc[:, 'Дата операции'][index], "%d.%m.%Y %H:%M:%S")
         date_formated = datetime.strftime(date, '%Y-%m-%d')
@@ -44,5 +61,6 @@ def preparing_list_transactions(my_list_transactions: pd.DataFrame) -> List[Dict
                 "Сумма операции": float(df.loc[:, 'Сумма операции'][index])
             }
         )
+    logger.info("Конец формирования списка")
 
     return result
