bd0e310b48753f93b7c7a81bb92ee41b9678685e
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-07-08 23:41:56 +0300

    add logging in module src/reports.py

--- src/reports.py
+++ src/reports.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from datetime import datetime
 from functools import wraps
@@ -6,6 +7,15 @@ from typing import Any, Callable, Optional
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 
+log_path = os.path.join(os.path.dirname(__file__), '..', 'logs', 'reports.log')
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler(log_path, mode='w', encoding='utf-8')
+file_formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
 
 def write_result_to_file(func: Callable) -> Any:
     """Декоратор для записи результатов в файл"""
@@ -13,6 +23,7 @@ def write_result_to_file(func: Callable) -> Any:
     @wraps(func)
     def wrapper(*args: Any, **kwargs: Any) -> Any:
         result: pd.DataFrame = func(*args, **kwargs)
+        logger.info("Запись данных в файл")
         result.to_json(
             os.path.join(os.path.dirname(__file__), "..", "data", "reports_result", "spending.json"), force_ascii=False
         )
@@ -29,6 +40,7 @@ def write_result_to_file_with_parameter(file_name: str = "result.json") -> Any:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
             result: pd.DataFrame = func(*args, **kwargs)
+            logger.info(f"Запись данных в файл с именем {file_name}")
             result.to_json(
                 os.path.join(os.path.dirname(__file__), "..", "data", "reports_result", file_name), force_ascii=False
             )
@@ -44,14 +56,17 @@ def write_result_to_file_with_parameter(file_name: str = "result.json") -> Any:
 def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame | str:
     """Возвращает траты по заданной категории за последние 3 месяца"""
 
+    logger.info("Вычисление начала и конца периода выборки")
     if date is None:
         stop_span = datetime.now()
     else:
         try:
             stop_span = datetime.strptime(date, "%d-%m-%Y %H:%M:%S")
         except ValueError:
+            logger.error("Передан неправильный формат даты")
             return "Неверный формат даты. Нужный формат - DD-MM-YYYY HH:MM:SS"
 
+    logger.info("Подготовка конечного DataFrame")
     start_span = stop_span - relativedelta(months=3)
 
     df = transactions[
